de60efaad07317d810d31693b2bc916a03520fd2
Hack Reactor Students <accounts@hackreactor.com>
2017-03-09 20:34:07 -0800

    Finish with bugs

--- app/config.js
+++ app/config.js
@@ -1,40 +1,72 @@
+var mongoose = require('mongoose');
 
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  },
-  useNullAsDefault: true
-});
-var db = require('bookshelf')(knex);
+mongoURI = 'mongodb://localhost/shortlydb';
+mongoose.connect(mongoURI);
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
-
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
+// Run in seperate terminal window using 'mongod'
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongodb connection open');
 });
 
 module.exports = db;
+// do not need. path module was being used for sqlite.
+// var path = require('path');
+
+
+// in the sql world, the database schema is encoded into the databasde itself
+// will be enforced in the sql database
+
+// mongodb doesn't enforce schema
+// in the mongo world, the database is a schemaless system
+// any schema is enforced at the database adapter level (which is mongoose)
+
+
+//but we will re-use this list of properties to define 
+//what we want to put into our mongoose database
+//       link.string('url', 255);
+//       link.string('baseUrl', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+
+// 'knex' was another module being used with sqlite.
+// var knex = require('knex')({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.join(__dirname, '../db/shortly.sqlite')
+//   },
+//   useNullAsDefault: true
+// });
+// var db = require('bookshelf')(knex);
+
+// db.knex.schema.hasTable('urls').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('urls', function (link) {
+//       link.increments('id').primary();
+//       link.string('url', 255);
+//       link.string('baseUrl', 255);
+//       link.string('code', 100);
+//       link.string('title', 255);
+//       link.integer('visits');
+//       link.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
+// db.knex.schema.hasTable('users').then(function(exists) {
+//   if (!exists) {
+//     db.knex.schema.createTable('users', function (user) {
+//       user.increments('id').primary();
+//       user.string('username', 100).unique();
+//       user.string('password', 100);
+//       user.timestamps();
+//     }).then(function (table) {
+//       console.log('Created Table', table);
+//     });
+//   }
+// });
+
--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,53 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+// there is no enforcement of a schema from the database
+// we enforce a schema from the database adapter
+
+var linkSchema = mongoose.Schema({
+  visits: Number,
+  link: String,
+  title: String,
+  code: String,
+  baseUrl: String,
+  url: String,
+  // hasTimestamps: {type: Boolean, default: true},
+  // timestamps: {type: Date, default: Date.now }
 });
+
+var Link = mongoose.model('Link', linkSchema);
+
+//similar hooks to bookshelf
+//replicates "initialize" - Mongo's version
+var createSha = function(url) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url);
+  return shasum.digest('hex').slice(0, 5);
+};
+
+linkSchema.pre('save', function(next) {
+  var code = createSha(this.url);
+  this.code = code;
+  next();
+});
+
+
+
+// linkSchema.query.remove = function(byUrl) {
+//   return this.findOneAndRemove(byUrl);
+// };
+
+// linkSchema.methods.fetch = function() {
+//   return this.model('Links').findOne({url: this.url}).exec();
+// };
+
+// linkSchema.methods.save = function() {
+//   return this.save();
+// };
+
+//Links = table name
+//We are using 'Links' name to create a table
+//based off the linkSchema we just defined
 
 module.exports = Link;
